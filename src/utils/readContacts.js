5169e47ea2a304eb0b9bc7c71f82bc078ba127cd
Anna Rybnikova <rybnikova.anna.18@gmail.com>
2025-01-11 00:32:18 +0100

    part 2 done

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -1,3 +1,11 @@
 import { PATH_DB } from '../constants/contacts.js';
+import fs from 'node:fs/promises';
 
-export const readContacts = async () => {};
+export const readContacts = async () => {
+    try {
+        const data = await fs.readFile(PATH_DB);
+        console.log('File content:', data);
+    } catch (err) {
+        console.error('File reading error:', err);
+    }
+};
