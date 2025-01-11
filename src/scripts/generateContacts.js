cf294b5c1cfca9451a649308c036421bc92e9943
Anna Rybnikova <rybnikova.anna.18@gmail.com>
2025-01-11 01:23:05 +0100

    step 3 done

--- src/scripts/generateContacts.js
+++ src/scripts/generateContacts.js
@@ -1,3 +1,21 @@
-const generateContacts = async (number) => {};
+import { createFakeContact } from "../utils/createFakeContact.js";
+import { readContacts } from "../utils/readContacts.js";
+import { writeContacts } from "../utils/writeContacts.js";
+
+const generateContacts = async (number) => {
+    try {
+        const contacts = await readContacts();
+        const fakedContacts = Array.from(
+            { length: number },
+            () => createFakeContact()
+        );
+        contacts.push(...fakedContacts);
+        
+        await writeContacts(contacts);
+        console.log(`Successfully generated contacts.`);
+    } catch (error) {
+        console.error('Error generating contacts:', error);
+    }
+};
 
 generateContacts(5);
