99d527985837c82483cdbaffafc8298315891815
Anna Rybnikova <rybnikova.anna.18@gmail.com>
2025-01-11 01:00:22 +0100

    update readContacts and writeContacts

--- src/utils/readContacts.js
+++ src/utils/readContacts.js
@@ -5,7 +5,10 @@ export const readContacts = async () => {
     try {
         const data = await fs.readFile(PATH_DB);
         console.log('File content:', data);
+        
+        return JSON.parse(data);
     } catch (err) {
         console.error('File reading error:', err);
+        throw new Error('Failed to read contacts data');
     }
 };
--- src/utils/writeContacts.js
+++ src/utils/writeContacts.js
@@ -3,9 +3,12 @@ import fs from 'node:fs/promises';
 
 export const writeContacts = async (updatedContacts) => {
     try {
-        await fs.writeFile(PATH_DB, updatedContacts, 'utf8');
+        const data = JSON.stringify(updatedContacts, null, 2);
+        
+        await fs.writeFile(PATH_DB, data, 'utf8');
         console.log('Data successfully written to the file.');
     } catch (err) {
-        console.error('Error writing to file:', err);
+        console.error('Error writing contacts to file:', err);
+        throw new Error('Failed to write contacts data');
     }
 };
